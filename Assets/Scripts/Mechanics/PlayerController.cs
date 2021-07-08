d9632d48909d04c4ebd946c297cca64bd1554e5b
Khalil <kjaxon6@gmail.com>
2021-07-08 16:56:21 -0700

    organized game objects

--- Assets/Scripts/Mechanics/PlayerController.cs
+++ Assets/Scripts/Mechanics/PlayerController.cs
@@ -12,6 +12,7 @@ namespace Platformer.Mechanics
     /// This is the main class used to implement control of the player.
     /// It is a superset of the AnimationController class, but is inlined to allow for any kind of customisation.
     /// </summary>
+    /// TODO: Add double jump so we have example code to show the students
     public class PlayerController : KinematicObject
     {
         public AudioClip jumpAudio;
